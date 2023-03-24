c70a8e386e70fea74d311e30d16f44f25fcefa14
Victor <victorze@protonmail.com>
2023-03-24 09:50:58 -0500

    Add code

---
+++ src/main/java/com/victorze/jwt/auth/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.victorze.jwt.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.victorze.jwt.config.JwtService;
+import com.victorze.jwt.user.Role;
+import com.victorze.jwt.user.User;
+import com.victorze.jwt.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/victorze/jwt/user/Role.java
@@ -0,0 +1,6 @@
+package com.victorze.jwt.user;
+
+public enum Role {
+    USER,
+    ADMIN,
+}
